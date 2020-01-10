3e10b5189a6f6e578ac660787cbcc5eed7b41196
Biswaranjan Rout <biswaranjan.rout1989@gmail.com>
2020-01-10 22:19:05 +0800

    Added problem

---
+++ problem-solving/same.js
@@ -0,0 +1,38 @@
+/*
+  Write a function called same, which accepts two arrays. 
+  The function should return true if every value in the array has it's corresponding value squared in the second array. The frequency of values must be the same. 
+  same([1, 2, 3], [4, 1, 9]) // true
+  same([1, 2, 3], [1, 9]) // false
+  same([1, 2, 1], [4, 4, 1]) // false (must be same frequency)
+*/
+
+// Solved using pattern - Frequency Counter
+
+const same = (arr1, arr2) => {
+  // Edge case
+  if (arr1.length != arr2.length) return false;
+
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+
+  for (let val of arr1) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+
+  for (let key in frequencyCounter1) {
+    if (
+      !(key ** 2 in frequencyCounter2) ||
+      frequencyCounter2[key ** 2] !== frequencyCounter1[key]
+    ) {
+      return false;
+    }
+  }
+};
+
+console.log(same([1, 2, 3], [4, 1, 9]));
+console.log(same([1, 2, 3], [1, 9]));
+console.log(same([1, 2, 1], [4, 4, 1]));
