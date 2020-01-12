61cf5101edfbf61931ef46929a96718090d3ff43
Biswaranjan Rout <biswaranjan.rout1989@gmail.com>
2020-01-13 01:02:42 +0800

    Added new problem

--- recursion-examples/fibonacci.js
+++ recursion-examples/fibonacci.js
@@ -2,9 +2,21 @@
  * Return the nth number of fibonacci series using recursion
  */
 
-const fibonacci = n => {
-  if (n <= 2) return 1;
-  return fibonacci(n - 1) + fibonacci(n - 2);
+const fibonacciRecursive = n => {
+  if (n == 1 || n == 2) return 1;
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
 };
 
-console.log(fibonacci(6));
+console.log(fibonacciRecursive(5));
+
+const memo = [];
+const fibonacciMemoize = n => {
+  if (memo[n] !== undefined) return memo[n];
+  let result;
+  if (n === 1 || n === 2) result = 1;
+  else result = fibonacciMemoize(n - 1) + fibonacciMemoize(n - 2);
+  memo[n] = result;
+  return result;
+};
+
+console.log(fibonacciRecursive(5));
