3018fc50a76ffe3da8d3a06ecd16d6f3595a85a0
Biswaranjan Rout <biswaranjan.rout1989@gmail.com>
2020-01-14 21:21:54 +0800

    Added new problem

---
+++ linked-list/linkedList.js
@@ -0,0 +1,56 @@
+/**
+ * Implement linked list
+ */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+    }
+    return current.val;
+  }
+}
+
+const list = new SinglyLinkedList();
+list.push("🙏🏻");
+list.push("👋🏻");
+list.push("👍");
+list.push("🐀");
+
+console.log(list.pop());
