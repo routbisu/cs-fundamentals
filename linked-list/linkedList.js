37deacff81383a80269fb93517e092be28eaf398
Biswaranjan Rout <biswaranjan.rout1989@gmail.com>
2020-01-14 22:42:51 +0800

    Added new problem

--- linked-list/linkedList.js
+++ linked-list/linkedList.js
@@ -42,15 +42,101 @@ class SinglyLinkedList {
     this.length--;
     if (this.length === 0) {
       this.head = null;
+      this.tail = null;
     }
     return current.val;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    this.head = current.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return current.val;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+
+    if (this.length === 1) {
+      this.tail = newNode;
+    }
+
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
+
+  set(index, val) {
+    const foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return this.push(val);
+    if (index === 0) return this.unshift(val);
+
+    const newNode = new Node(val);
+    const prevNode = this.get(index - 1);
+    const temp = prevNode.next;
+    prevNode.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    let previousNode = this.get(index - 1);
+    let removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
   }
 }
 
 const list = new SinglyLinkedList();
-list.push("🙏🏻");
-list.push("👋🏻");
-list.push("👍");
-list.push("🐀");
+list.push("Pratham");
+list.push("Dwitiya");
+list.push("Tritiya");
+list.push("Chaturth");
 
-console.log(list.pop());
+// console.log(list.pop());
+// console.log(list.pop());
+// console.log(list.pop());
+// console.log(list.pop());
+// console.log(list.shift());
+// console.log(list.shift());
+// console.log(list.shift());
+// console.log(list.shift());
+console.log(list.unshift("Extra"));
+// console.log(list.shift());
+console.log("Get", list.get(1));
+console.log("Set", list.set(1, "Pratham2"));
+console.log("Set", list.insert(1, "Before Pratham"));
+console.log("Set", list.remove(1, "Before Pratham"));
+console.log(list);
